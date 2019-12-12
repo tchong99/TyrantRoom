e8201654a92b2ded37ed92df103263c8c27ce584
Tim Chong <tchong99@uw.edu>
2019-12-12 13:43:38 -0800

    #DOOOOR
    go in and out of worlds with this nifty door!

--- Assets/TyrantRoom/Scripts/Door.cs
+++ Assets/TyrantRoom/Scripts/Door.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
@@ -12,6 +13,12 @@ public class Door : MonoBehaviour
     public bool access1;
     public bool access2;
     public bool access3;
+    public bool doorOpen;
+
+    public Material doorDoneColor;
+
+    public string levelToLoad;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,19 +26,31 @@ public class Door : MonoBehaviour
         access1 = false;
         access2 = false;
         access3 = false;
+        doorOpen = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         access1 = key1.GetComponent<Key>().getTriggered();
-        access2 = key1.GetComponent<Key>().getTriggered();
-        access3 = key1.GetComponent<Key>().getTriggered();
+        access2 = key2.GetComponent<Key>().getTriggered();
+        access3 = key3.GetComponent<Key>().getTriggered();
 
         //print("Dooropen!!!");
         if (access1 && access2 && access3)
         {
-            print("Dooropen!!!");
+            //print("Dooropen!!!");
+            doorOpen = true;
+            gameObject.GetComponent<MeshRenderer>().material = doorDoneColor;
+
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player") && doorOpen) 
+        {
+            SceneManager.LoadScene(levelToLoad);
         }
     }
 }
